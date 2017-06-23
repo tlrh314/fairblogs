25c01f9041848f43e6565b46c8e682e01043b34c
Marcella Wijngaarden <marcella.wijngaarden@student.uva.nl>
2017-06-23 23:50:32 +0200

    post model, initial template setup

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):
+    def make_url_id(self):
+        return self.title.replace(" ", "-")
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, default=None)
+    title = models.CharField(max_length=300)
+    blogintro = models.TextField(default=" ", help_text="Write the short introduction of the blog here, it will be displayed in de bloglist")
+    image = models.CharField(max_length=500, default="Add image here", help_text="Write in format: 'filename.png' and put image in static/img/blog folder")
+
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            default=timezone.now, blank=True, null=True)
+
+
+    tag = models.CharField(max_length=300) # models.ForeignKey(Category, help_text='Category', default=1)
+
+    featured = models.BooleanField(default=False,
+            help_text="Should this post be shown in the featured list?")
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    class Meta:
+        ordering = ['-published_date',]
+
+    def post_ID(self):
+        '''
+        Returns url-ID for blog posts: the title with only alphanumeric
+        charachters and the words seperated by -. For example:
+        'Example, title of a post 1.' becomes 'example-title-of-a-post-1'.
+        '''
+        a = self.title
+        a = pattern.sub('-', a)
+        if a.endswith('-'):
+            a = a[:-1]
+        return a.lower()
+
+    def __str__(self):
+        return self.title
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django import template
+from .models import *
 
 # Create your views here.
+register = template.Library()
+
+def index(request):
+    blogs = Post.objects.all()
+
+    return render(request, 'blog/index.html', {'blogs': blogs})
