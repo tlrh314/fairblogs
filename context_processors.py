725037ce573729d3f89e8c3ecdafe06a2b8b6ee8
Timo Halbesma <timohalbesma@gmail.com>
2017-06-25 20:52:47 +0200

    Fix when database does not yet exist

--- context_processors.py
+++ context_processors.py
@@ -3,21 +3,31 @@ from __future__ import unicode_literals, absolute_import, division
 from apps.pages.models import ContactInfo
 from apps.blog.models import Tag
 
+from django.db.utils import OperationalError
+
+
+class ContactInfoDefault(object):
+    def __init__(self):
+        self.contact_email = "hello@fairblogs.nl"
+        self.webmaster_email = "hello@fairblogs.nl"
+        self.address = "Oetewalerstraat 83"
+        self.postbox = "Amsterdam, 1093 ME"
+        self.phone = "0031683210121"
+
 
 def contactinfo(request):
     contactinfo = ContactInfo.objects.all()
-    if contactinfo:
-        contactinfo = contactinfo[0]
+    try:
+        if contactinfo:
+            contactinfo = contactinfo[0]
+            p = contactinfo.phone
+        else:
+            contactinfo = ContactInfoDefault()
+            p = contactinfo.phone
+    except OperationalError as e:
+        print("Database was not yet created. Make migrations and migrate first.")
+        contactinfo = ContactInfoDefault()
         p = contactinfo.phone
-    else:
-        contactinfo = dict()
-        # Hardcoded in case ContactInfo has no instances.
-        contactinfo["contact_email"] = "hello@fairblogs.nl"
-        contactinfo["webmaster_email"] = "hello@fairblogs.nl"
-        contactinfo["address"] = "Oetewalerstraat 83"
-        contactinfo["postbox"] = "Amsterdam, 1093 ME"
-        contactinfo["phone"] = default="0031683210121"
-        p = contactinfo.get("phone")
 
     phone_formatted = "+"+p[2:4]+" (0)"+p[4:6]+" "+p[6:9]+" "+p[9:11]+" "+p[11:13]
 
