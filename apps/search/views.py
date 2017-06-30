0b42fbb6be5b1e5b1f09226c401b756fddb50889
Timo Halbesma <timohalbesma@gmail.com>
2017-07-01 00:51:48 +0200

    Very ugly search functionality

--- apps/search/views.py
+++ apps/search/views.py
@@ -23,62 +23,73 @@ def search(request):
     words = request.GET.get("terms", "")
     terms, posts, bloggers, affiliated_blogs = [], [], [], []
 
-    # if len(words) <= 2:
-    #     msg = "Please use at least 3 characters to search. "
-    #     msg += "Tip: you can use exact match by placing ' or \" around words!"
-    #     messages.error(request, msg)
-    #     return render(request, "search/search_results.html", {"alumni": [], "key_words": []})
+    if len(words) <= 2:
+        messages.error(request, "Error: Please use at least 3 characters to search.")
+        messages.info(request, "Tip: you can use exact match by placing ' or \" around words!")
+        return render(request, "search/search_results.html",
+            {
+                "posts": posts,
+                "bloggers": bloggers,
+                "affiliated_blogs": affiliated_blogs,
+                "key_words": terms
+            }
+        )
 
-    # if len(words.split()) > 10:
-    #     msg = "Please limit your search to <10 words. "
-    #     msg += "Tip: you can use exact match by placing ' or \" around words!"
-    #     messages.error(request, msg)
-    #     return render(request, "search/search_results.html", {"alumni": [], "key_words": []})
+    if len(words.split()) > 10:
+        messages.error(request, "Error: Please limit your search to <10 words.")
+        messages.info(request, "Tip: you can use exact match by placing ' or \" around words!")
+        return render(request, "search/search_results.html",
+            {
+                "posts": posts,
+                "bloggers": bloggers,
+                "affiliated_blogs": affiliated_blogs,
+                "key_words": terms
+            }
+        )
 
-    # # Check if must be exact match (i.e. if between quotation marks)
-    # words = words.replace("'", '"')
-    # if '"' in words:
-    #     exact_match = re.findall('"([^"]*)"', words)
+    # Check if must be exact match (i.e. if between quotation marks)
+    words = words.replace("'", '"')
+    if '"' in words:
+        exact_match = re.findall('"([^"]*)"', words)
 
-    #     for exact in exact_match:
-    #         words.replace(exact, "")
-    #         terms.append(exact)
+        for exact in exact_match:
+            words.replace(exact, "")
+            terms.append(exact)
 
-    # # Create final lists of search terms
-    # terms = terms + words.split()
+    # Create final lists of search terms
+    terms = terms + words.split()
 
-    # # Set maximum number of words
-    # if len(terms) > 42:
-    #     return render(request, "search/search_results.html", {"alumni": [],
-    #                                                           "key_words": []})
-    # #  Remove single characters
-    # terms = [term for term in terms if len(term) > 1]
+    #  Remove single characters
+    terms = [term for term in terms if len(term) > 1]
 
-    # # Compute filters
-    # search_filter = Q()
-    # time_filter = Q()
-    # alumni = Alumnus.objects.all()
+    # Compute filters
+    posts_filter = Q()
+    bloggers_filter = Q()
+    affiliated_blogs_filter = Q()
+    all_posts = Post.objects.all()
+    all_bloggers = Blogger.objects.all()
+    all_affiliated_blogs = AffiliatedBlog.objects.all()
 
-    # for term in terms:
+    for term in terms:
+        posts_filter = (posts_filter |
+            Q(teaser__icontains=term) | Q(url__icontains=term) | Q(title__icontains=term)
+        )
 
-    #     # Check if year, if set time filters
-    #     if (term.isdigit() and len(term) == 4):
-    #         end_year = str(int(term) + 1)
-    #         date_range=[term+"-01-01",end_year+"-01-01"]
-    #         time_filter = (time_filter | Q(theses__date_of_defence__range=date_range)
-    #                                    | Q(theses__date_stop__range=date_range)
-    #                                    | Q(theses__date_start__range=date_range))
+        bloggers_filter = (bloggers_filter |
+            Q(first_name__icontains=term) | Q(last_name__icontains=term)
+        )
 
-    #     else:
-    #         search_filter = (search_filter | Q(last_name__icontains=term)|
-    #                                          Q(first_name__icontains=term) |
-    #                                          Q(theses__title__icontains=term))
+        affiliated_blogs_filter = (affiliated_blogs_filter |
+            Q(blogname__icontains=term) | Q(url__icontains=term)
+        )
 
     # # Compute combined filter
     # total_filter = time_filter & search_filter
 
     # # Apply all filters
-    # results = alumni.filter(total_filter).distinct()
+    posts = all_posts.filter(posts_filter).distinct()
+    bloggers = all_bloggers.filter(bloggers_filter).distinct()
+    affiliated_blogs = all_affiliated_blogs.filter(affiliated_blogs_filter).distinct()
 
     # if len(results) > 10:
     #     msg = "Search matched {0} items. Tip: you can use exact match by placing ' or \" around words!".format(len(results))
