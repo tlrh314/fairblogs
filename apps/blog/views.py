209b7617dafb3fd969f9f4bf808f43dcc0158729
Nelle Kooren <nellekooren@gmail.com>
2017-09-03 17:15:32 +0200

    pub_date is date_created

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -187,7 +187,7 @@ class PostsFeed(Feed):
     description = "Posts from FairBlogs"
 
     def items(self):
-        return Post.objects.order_by('pub_date')[:10]
+        return Post.objects.order_by('date_created')[:10]
 
     def item_title(self, item):
         return item.title
