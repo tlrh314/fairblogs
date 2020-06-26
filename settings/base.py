faa892186e40608684e9244799eec377ee5021a6
Timo Halbesma <timohalbesma@gmail.com>
2020-06-27 00:51:48 +0200

    Do not set MEDIA_ROOT and MEDIA_URL to fix broken links

--- settings/base.py
+++ settings/base.py
@@ -82,8 +82,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
 ]
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 ALLOWED_HOSTS = [
     "localhost", "127.0.0.1", "testserver", "fairblogs.nl", "www.fairblogs.nl",
